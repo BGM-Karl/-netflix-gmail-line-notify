3fa01be8023cd82b4973e905339ab7c7f6047e63
Karl <karl2015y@gmail.com>
2023-03-05 23:22:03 +0800

    更新腳本

--- start_preview.sh
+++ start_preview.sh
@@ -2,4 +2,6 @@ cd /home/ec2-user # 切換到您的應用程式程式碼所在的目錄
 rm -rf myapp # 刪除舊的檔案
 mkdir myapp
 unzip -o -q app.zip -d myapp  # 解壓縮 
+chmod 777 -R myapp
+mv myapp/node_modules/ myapp/dist/node_modules
 # pm2 start dist/app-bundle.js # 用 pm2 執行腳本
