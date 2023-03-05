34c55faac63fde16e86282af4a802de69ec7c7df
Karl <karl2015y@gmail.com>
2023-03-05 22:48:34 +0800

    切換根目錄

--- start_preview.sh
+++ start_preview.sh
@@ -1,5 +1,5 @@
+cd /home/ec2-user # 切換到您的應用程式程式碼所在的目錄
 rm -rf myapp # 刪除舊的檔案
 mkdir myapp
-# cd /home/ec2-user/myapp # 切換到您的應用程式程式碼所在的目錄
 unzip -o -q app.zip -d myapp  # 解壓縮 
 # pm2 start dist/app-bundle.js # 用 pm2 執行腳本
